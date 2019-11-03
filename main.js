e8588335a0a9906a9d758ca9ff630dee4db77fd2
Yi Fei Cheng <ycheng2@swarthmore.edu>
2019-11-03 10:24:40 +0000

    Implemented basic Phong shading.

--- main.js
+++ main.js
@@ -6,11 +6,16 @@ RESOURCES = [
   // format is:
   // ["name", "path-to-resource"]
   ["vert_shader", "shaders/default.vert"],
-  ["frag_shader", "shaders/default.frag"]
+  ["frag_shader", "shaders/default.frag"],
+  ["frag_phong", "shaders/phong.frag"],
+  ["vert_phong", "shaders/phong.vert"]
 ]
 
-/* 
-    
+// Import Utah Teapot geometry
+// Source: Udacity Interactive 3D Graphics (https://threejs.org/examples/webgl_geometry_teapot.html)
+
+/*
+
     Create the scene
 
 */
@@ -28,14 +33,45 @@ function initialise_scene(resources) {
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
+  // Camera controls
+  var controls = new THREE.OrbitControls( camera, renderer.domElement );
+  camera.position.set( 0, 0, 25 );
+  controls.update();
 
-  // Add things to your scene here
+  // Lighting
+  var light = new THREE.PointLight( 0xffffff, 1, 100 );
+  light.position.set( 10, 10, 10 );
+  scene.add( light );
 
-  // [...]
+  // Utah Teapot
+  var teapotGeometry = new THREE.TeapotBufferGeometry(5, 10, true, true, true, false, true);
+  var teapotMaterial = new THREE.ShaderMaterial({
+    vertexShader: resources.vert_phong,
+    fragmentShader: resources.frag_phong,
+    uniforms: {
+      lightPos: {value: light.position},
+      lightColor: {value: new THREE.Vector3(1.0,1.0,1.0)},
+      color: {value: new THREE.Vector3(1.0,1.0,1.0)},
+      ambientStrength: {value: 0.25},
+      diffuseStrength: {value: 0.5},
+      specularStrength: {value: 0.5},
+      phongExponent: {value: 32.0},
+      viewPos: {value: camera.position}
+    }
+  });
+  teapotMaterial.needsUpdate = true;
+  var teapot = new THREE.Mesh(teapotGeometry, teapotMaterial);
+  scene.add(teapot);
 
   // Your animation loop, which will run repeatedly and renders a new frame each time
   var animate = function () {
     requestAnimationFrame( animate );
+
+    // Update teapot material accordingly
+    teapot.material.uniforms.viewPos.value = camera.position;
+    // Update controls
+    controls.update();
+
     renderer.render( scene, camera );
   };
 
--- shaders/phong.vert
+++ shaders/phong.vert
@@ -4,4 +4,5 @@ void main() {
   world_normal = normalize( mat3( modelMatrix[0].xyz, modelMatrix[1].xyz, modelMatrix[2].xyz ) * normal );
 
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+  fragPos = vec3(gl_Position.x, gl_Position.y, gl_Position.z);
 }
